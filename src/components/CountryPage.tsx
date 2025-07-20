089269aaa89c261635e92eea3faf93ff84d51da4
айхан-гаджибалаев <ayhan.gadjibalayev@gmail.com>
2025-07-20 17:03:52 +0400

    9 the task is completed

--- src/components/CountryPage.tsx
+++ src/components/CountryPage.tsx
@@ -1,31 +1,57 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-export function CountryPage() {
-  type Country = {
-    name: {
-      common: string;
-    };
+type Country = {
+  name: {
+    common: string;
   };
+  flags: {
+    png: string;
+  };
+  capital: {
+    0: string;
+  };
+  region: string;
+  languages: Record<string, string>;
+  currencies: Record<string, { name: string; symbol: string }>;
+};
 
+export function CountryPage() {
   const { name } = useParams<{ name: string }>();
   const [country, setCountry] = useState<Country | null>(null);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`https://restcountries.com/v3.1/name/${name}`);
-        const [data] = await response.json();
-        setCountry(data);
-      } catch (error) {}
-    };
+    // const fetchData = async () => {
+    try {
+      fetch(`https://restcountries.com/v3.1/name/${name}`)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data: Country[]) => {
+          setCountry(data[0]);
+          // setLoading(false);
+          console.log(data);
+        });
+    } catch (error) {}
+    // };
 
-    fetchData();
-  });
+    // fetchData();
+  }, []);
 
   return (
-    <div>
-      <span>Country {country?.name.common}</span>
+    <div className="country-page">
+      <img src={country?.flags.png} alt="" className="flag" />
+      <div className="country-info-block">
+      <span style={{fontSize:'20px', fontWeight:'700'}}>{country?.name.common}</span>
+      <span>Capital: {country?.capital[0]}</span>
+      <span>Currencies: {country?.currencies && Object.values(country.currencies).map(currency => `${currency.name} (${currency.symbol})`).join(", ")}</span>
+      <span>Languages:{" "}{country?.languages && Object.values(country.languages)}</span>
+      <span> Region: {country?.region}</span>
+      </div>
     </div>
   );
 }
+
